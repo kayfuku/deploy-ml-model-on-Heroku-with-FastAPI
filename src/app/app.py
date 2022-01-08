303533029d4932f29d756e09c3a3d3f77b7ff043
Kei Fukutani <kifinewera@gmail.com>
2022-01-08 18:27:01 +0900

    add test_predict()

--- src/app/app.py
+++ src/app/app.py
@@ -3,7 +3,6 @@ FastAPI routing functions
 Author: Kei
 Date: January, 2022
 """
-import os
 from fastapi.param_functions import Body
 import yaml
 import joblib
@@ -37,8 +36,15 @@ async def predict(person: Person = Body(..., examples=examples)):
         [person[f] for f in examples['features_columns']]).reshape(1, -1)
     df = pd.DataFrame(features, columns=examples['features_columns'])
 
-    pred_label = model.predict(df)
-    pred_probs = model.predict_proba(df)[:, 1]
+    pred_label = model.predict(df)[0]
+    # print(pred_label)
+    pred_probs = model.predict_proba(df)[0][pred_label]
+    # print(pred_probs)
     pred = '>50k' if pred_label == 1 else '<=50k'
+    # print(pred)
+    ret = {
+        "label": str(pred_label),
+        "prob": str(pred_probs),
+        "salary": str(pred)}
 
-    return {'label': pred_label, 'prob': pred_probs, 'salary': pred}
+    return ret
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -13,8 +13,29 @@ client = TestClient(app)
 
 def test_greetings():
     """
-    Test GET /
+    Test greetings function, GET /
     """
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == "Hello, welcome!"
+
+
+def test_predict():
+    """
+    Test predict function, POST /predict
+    """
+    data = {
+        'age': 40,
+        'fnlgt': 1000,
+        'education_num': 14,
+        'capital_gain': 200,
+        'capital_loss': 0,
+        'hours_per_week': 40,
+    }
+    response = client.post('/predict', json=data)
+    # print(response.json())
+    assert response.status_code == HTTPStatus.OK
+    assert int(response.json()['label']) == 0 or \
+        int(response.json()['label']) == 1
+    assert float(response.json()['prob']) >= 0 and \
+        float(response.json()['label']) <= 1
