8735e3aa4dcf1b877543c82dc371b926ca7a5f6a
Kei Fukutani <kifinewera@gmail.com>
2022-01-14 16:51:59 +0900

    add test cases for label 1 and 0.

--- src/app/app.py
+++ src/app/app.py
@@ -60,11 +60,12 @@ async def predict(person: Person = Body(..., example=examples['post_examples']))
     pred_probs = model.predict_proba(df)[0][pred_label]
     # print(pred_probs)
     pred = '>50k' if pred_label == 1 else '<=50k'
-    print(pred)
+    print("pred:", pred)
     ret = {
         "label": str(pred_label),
         "prob": str(pred_probs),
         "salary": str(pred)
     }
+    print("predicted label:", ret['label'])
 
     return ret
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -19,25 +19,56 @@ def test_greetings():
     assert response.json() == "Hello, welcome!"
 
 
-def test_predict():
+def test_predict_label_1():
     """
-    Test predict function, POST /predict
+    Test predict function, POST /predict, with data to predict label 1
     """
-    data = {
-        'age': 40,
-        'fnlgt': 1000,
-        'education_num': 14,
-        'capital_gain': 200,
-        'capital_loss': 0,
-        'hours_per_week': 40,
-    }
+    data = {'age': 41,
+            'workclass': 'Private',
+            'fnlgt': 153031,
+            'education': 'Some-college',
+            'education_num': 10,
+            'marital_status': 'Married-civ-spouse',
+            'occupation': 'Sales',
+            'relationship': 'Husband',
+            'race': 'White',
+            'sex': 'Male',
+            'capital_gain': 0,
+            'capital_loss': 0,
+            'hours_per_week': 65,
+            'native_country': 'United-States',
+            }
+
     response = client.post('/predict', json=data)
     # print(response.json())
     assert response.status_code == HTTPStatus.OK  # 200
-    assert int(response.json()['label']) == 0 or \
-        int(response.json()['label']) == 1
-    assert float(response.json()['prob']) >= 0 and \
-        float(response.json()['label']) <= 1
+    assert int(response.json()['label']) == 1
+
+
+def test_predict_label_0():
+    """
+    Test predict function, POST /predict, with data to predict label 0
+    """
+    data = {'age': 26,
+            'workclass': 'Private',
+            'fnlgt': 108019,
+            'education': 'HS-grad',
+            'education_num': 9,
+            'marital_status': 'Never-married',
+            'occupation': 'Craft-repair',
+            'relationship': 'Own-child',
+            'race': 'White',
+            'sex': 'Male',
+            'capital_gain': 3325,
+            'capital_loss': 0,
+            'hours_per_week': 40,
+            'native_country': 'United-States'
+            }
+
+    response = client.post('/predict', json=data)
+    # print(response.json())
+    assert response.status_code == HTTPStatus.OK  # 200
+    assert int(response.json()['label']) == 0
 
 
 def test_invalid_predict():
